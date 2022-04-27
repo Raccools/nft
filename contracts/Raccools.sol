3658404415e7b2ae99d4a5c8b9a14521d37548e4
Felipe Lincoln <felipelincoln@usp.br>
2022-04-26 22:23:07 -0300

    create custom trait method

--- contracts/Raccools.sol
+++ contracts/Raccools.sol
@@ -15,36 +15,44 @@ contract Raccools {
   string public _provenance;
 
   // trait name and svg image
-  string[2][2] private _backgroundTraits = [
+  string[2][4] private _backgroundTraits = [
     ["yellow", "svg1"],
     ["blue", "svg2"]
   ];
 
-  string[2][2] private _furTraits = [
+  string[2][4] private _furTraits = [
     ["basic 1", "svga"],
     ["basic 2", "svgb"]
   ];
 
-  string[2][2] private _faceTraits = [
+  string[2][4] private _faceTraits = [
     ["smile", "svgX"],
     ["yum", "svgY"]
   ];
 
-  string[2][2] private _headTraits = [
+  string[2][4] private _headTraits = [
+    ["", ""],
+    ["", ""],
     ["mohawk", "svg=="],
     ["flower", "svg+="]
   ];
 
-  string[2][2] private _clothesTraits = [
+  string[2][4] private _clothesTraits = [
+    ["", ""],
+    ["", ""],
     ["suit", "svggg"],
     ["bag", "svgff"]
   ];
 
-  uint256[2] private _backgroundRarities = [5, 5];
-  uint256[2] private _furRarities = [5, 5];
-  uint256[2] private _faceRarities = [5, 5];
-  uint256[2] private _headRarities = [5, 5];
-  uint256[2] private _clothesRarities = [5, 5];
+  // generator rarities
+  uint256[4] private _backgroundRarities = [5, 5];
+  uint256[4] private _furRarities = [5, 5];
+  uint256[4] private _faceRarities = [5, 5];
+  uint256[4] private _headRarities = [0, 0, 5, 5];
+  uint256[4] private _clothesRarities = [0, 0, 5, 5];
+
+  mapping(uint256 => uint256) _customHead;
+  mapping(uint256 => uint256) _customClothes;
 
   constructor(){
     console.log(background(0)[0]);
@@ -67,14 +75,21 @@ contract Raccools {
   }
 
   function head(uint256 tokenId_) private view returns(string[2] memory){
-    return _headTraits[generateTrait(tokenId_, _headRarities)];
+    return _headTraits[customTrait(tokenId_, _headRarities, _customHead)];
   }
 
   function clothes(uint256 tokenId_) private view returns(string[2] memory){
-    return _clothesTraits[generateTrait(tokenId_, _clothesRarities)];
+    return _clothesTraits[customTrait(tokenId_, _clothesRarities, _customClothes)];
   }
 
-  function generateTrait(uint256 tokenId_, uint256[2] memory rarities_) private view returns(uint256){
+  function customTrait(uint256 tokenId_, uint256[4] memory rarities_, mapping(uint256 => uint256) storage customs_) private view returns(uint256){
+    uint256 n = customs_[tokenId_];
+    if(n == 0) return generateTrait(tokenId_, rarities_);
+
+    return n;
+  }
+
+  function generateTrait(uint256 tokenId_, uint256[4] memory rarities_) private view returns(uint256){
     uint256 n = 1 + random(tokenId_.toString()) % 10;
     uint256 trait = 0;
 
